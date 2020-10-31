c27aa943a44b5fb1f0c2cd4a5bfb95cd6ccac50c
SuyashKejriwal <suyashkejriwal1308@gmail.com>
2020-10-31 12:54:40 +0530

    Connecting Backend to Frontend using Axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import products from '../Products'
+import React, { useState,useEffect } from 'react'
 import Product from '../components/Product'
-import {Row,Col,Container} from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+
 const HomeScreen = () => {
-    return (
+    const [products, setProducts] = useState([])
+    
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products');
+            setProducts(data);
+        }
+
+        fetchProducts()
+    },[])
+    return ( 
         <>
             <h1>Latest Products</h1>  
             <Row>
